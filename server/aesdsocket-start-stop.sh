f85209f101c8dd96b9f11a6c1042d6560cfd3c5f
saf <saf@ubuntu2.myguest.virtualbox.org>
2023-10-04 16:57:21 +0400

    modified start-stop

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -1,17 +1,19 @@
-#! /bin/sh
+#!/bin/sh
 
 case "$1" in
     start)
-        echo "Starting aesdsocket"
-        start-stop-daemon -S -n aesdsocket -- /usr/bin/aesdsocket -d
+        echo "starting simple server"
+        start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
         ;;
+
     stop)
-        echo "Stopping aesdsocket"
-        start-stop-daemon -K -n aesdsocket
+        echo "stopping simple server"
+        start-stop-daemon -K -n aesdsocket --signal TERM
         ;;
+
     *)
         echo "Usage: $0 {start|stop}"
-        exit 1
+    exit 1
 esac
 
 exit 0
